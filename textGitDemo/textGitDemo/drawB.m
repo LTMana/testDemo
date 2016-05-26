f32082e5e298c0d5196bece74d1644f4be5bd9b9
LTMana <30500660@qq.com>
2016-05-26 09:29:05 +0800

    添加画页迁移项目

--- textGitDemo/textGitDemo/drawB.m
+++ textGitDemo/textGitDemo/drawB.m
@@ -1,40 +1,38 @@
 //
-//  ViewController.m
-//  textGitDemo
+//  drawB.m
+//  
 //
-//  Created by 刘博通 on 16/5/10.
-//  Copyright (c) 2016年 ltcom. All rights reserved.
+//  Created by 刘博通 on 16/5/11.
+//
 //
 
-#import "ViewController.h"
 #import "drawB.h"
-@interface ViewController ()
 
-@end
+@implementation drawB
 
-@implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-//    drawB *d=[[drawB alloc]init];
-//    d.frame =self.view.bounds;
-//    
-//    [self.view addSubview:d];
-    // Do any additional setup after loading the view, typically from a nib.
-        [self.view layoutIfNeeded];
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+//        UIColor *color =[UIColor redColor];
+//        self.backgroundColor =color;
+
+    }
+    return self;
 }
 
 
-- (void)drawRect:(CGRect)rect {
 
-    self.view.frame =rect;
+- (void)drawRect:(CGRect)rect {
     UIBezierPath *mPath = [UIBezierPath bezierPath];
     [mPath moveToPoint:CGPointMake(100, 100)]; //创建一个点
     [mPath addLineToPoint:CGPointMake(100, 300)]; // 加条线,从点移动到另一个点
     [mPath addLineToPoint:CGPointMake(300, 300)]; // 加条线,从点移动到另一个点
     [mPath closePath]; // 关闭贝塞尔线
     
-    
+   
     UIColor *fillColor = [UIColor greenColor];          //设置颜色
     [fillColor set];                                    //填充颜色
     [mPath fill];                                       //贝塞尔线进行填充
@@ -43,19 +41,15 @@
     [stroke set];                                       //填充颜色
     [mPath stroke];
     
-    
-}
+  }
+
+
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    [self.view setNeedsDisplay];
-    
+    self.backgroundColor =[UIColor whiteColor];
+    [self layoutIfNeeded];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
---
+++ 画页迁移/画页迁移/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  画页迁移
+//
+//  Created by 刘博通 on 16/5/26.
+//  Copyright (c) 2016年 ltcom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
