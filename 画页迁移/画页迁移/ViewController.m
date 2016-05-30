9c9e290a6d6279aca6a586fddf5e9293f896e73a
LTMana <30500660@qq.com>
2016-05-30 17:13:26 +0800

    添加一些demo

--- 画页迁移/画页迁移/ViewController.m
+++ 画页迁移/画页迁移/ViewController.m
@@ -16,12 +16,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"TabViewDidLoad-加载视图");
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSLog(@"TabViewWillAppear-视图将要显示");
 }
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSLog(@"TabViewDidAppear-视图已经显示");
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    NSLog(@"TabViewWillDisappear-视图将要消失");
+}
+
+-(void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    NSLog(@"TabViewDidDisappear-视图已经消失");
+}
+
 
 @end
